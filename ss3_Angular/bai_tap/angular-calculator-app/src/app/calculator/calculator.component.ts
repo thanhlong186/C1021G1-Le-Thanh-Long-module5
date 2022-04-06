acecde7008377c0ebeddb0fecff7a2f265350db0
thanhlong186 <thanhlong180696@gmail.com>
2022-04-06 11:28:54 +0700

    push

--- ss3_Angular/bai_tap/angular-calculator-app/src/app/calculator/calculator.component.ts
+++ ss3_Angular/bai_tap/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {toNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_version';
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  result: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  // opertion(num1: number, num2: number, operator: string){
+  //
+  //   switch (operator) {
+  //     case '+':
+  //       this.result = num1 + num2;
+  //       break;
+  //     case '-':
+  //       this.result = num1 - num2;
+  //       break;
+  //     case '*':
+  //       this.result = num1 * num2;
+  //       break;
+  //     case '/':
+  //       this.result = num1 / num2;
+  //       break;
+  //   }
+  // }
+  opertion(value: string, value2: string, value3: string) {
+
+      switch (value3) {
+        case '+':
+          this.result = Number(value) + Number(value2);
+          break;
+        case '-':
+          this.result = Number(value) - Number(value2);
+          break;
+        case '*':
+          this.result = Number(value) * Number(value2);
+          break;
+        case '/':
+          this.result = Number(value) / Number(value2);
+          break;
+      }
+
+  }
 }
